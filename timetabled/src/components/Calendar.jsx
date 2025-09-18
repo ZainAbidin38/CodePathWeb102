891c6d13129d3c74722fe56c3a679138da5f42ee
ZainAbidin38 <144952346+ZainAbidin38@users.noreply.github.com>
2025-09-17 21:55:38 -0400

    Refactor App component and add Calendar component with initial layout

---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,35 @@
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <td className="time">9 am</td>
+                        <td className="time">10 am</td>
+                        <td className="time">11 am</td>
+                        <td className="time">12 pm</td>
+                        <td className="time">1 pm</td>
+                        <td className="time">2 pm</td>
+                        <td className="time">3 pm</td>
+                        <td className="time">4 pm</td>
+                        <td className="time">5 pm</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar
